048efb606bf1fb441b9ddd30576d7c4afb0360f6
renald.fejzulla@gmail.com <renald.fejzulla@gmail.com>
2023-03-05 13:50:56 +0100

    Commit

--- ParkingLotManagament/BLL/Services/CodeGenerator.cs
+++ ParkingLotManagament/BLL/Services/CodeGenerator.cs
@@ -1,39 +1,17 @@
-﻿using System.Text;
-using XSystem.Security.Cryptography;
-
+﻿
 namespace ParkingLotManagament.BLL.Services
 {
     public class CodeGenerator
     {
         public static string SubscribtionGenerateCode(DateTime startDate, int userId, int Subscriberid, decimal price, DateTime endDate)
         {
-            // Concatenate the input values into a single string
-            string inputString = $"{startDate.ToString("yyyyMMdd")}-{userId}--{Subscriberid}-{price.ToString("#.##")}-{endDate.ToString("yyyyMMdd")}";
-
-            // Create a hash value from the input string
-            byte[] inputBytes = Encoding.UTF8.GetBytes(inputString);
-            byte[] hashBytes = new SHA256Managed().ComputeHash(inputBytes);
-
-            // Convert the hash bytes to a hexadecimal string
-            string code = BitConverter.ToString(hashBytes).Replace("-", "");
-
-            // Return the resulting code
+            var code = $"{Subscriberid}-{startDate.ToString("yyyyMMdd")}-{userId}-{price.ToString("F2")}-{endDate.ToString("yyyyMMdd")}";
             return code;
         }
 
         public static string Subscriber(DateTime startDate, int userId, decimal price, DateTime endDate)
         {
-            // Concatenate the input values into a single string
-            string inputString = $"{startDate.ToString("yyyyMMdd")}-{userId}-{price.ToString("#.##")}-{endDate.ToString("yyyyMMdd")}";
-
-            // Create a hash value from the input string
-            byte[] inputBytes = Encoding.UTF8.GetBytes(inputString);
-            byte[] hashBytes = new SHA256Managed().ComputeHash(inputBytes);
-
-            // Convert the hash bytes to a hexadecimal string
-            string code = BitConverter.ToString(hashBytes).Replace("-", "");
-
-            // Return the resulting code
+            var code = $"{startDate.ToString("yyyyMMdd")}-{userId}-{price.ToString("F2")}-{endDate.ToString("yyyyMMdd")}";
             return code;
         }
 
