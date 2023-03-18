4827c79b30df62d431971a7ca123b517966360ee
renald.fejzulla@gmail.com <renald.fejzulla@gmail.com>
2023-03-18 11:48:39 +0100

    commit

--- ParkingLotManagament/Models/Log.cs
+++ ParkingLotManagament/Models/Log.cs
@@ -7,7 +7,7 @@ namespace ParkingLotManagament.Models
     {
         public int Id { get; set; }
         public string Code { get; set; }
-        public int SubscriptionId { get; set; }
+        public int? SubscriptionId { get; set; }
         public DateTime CheckInTime { get; set; }
         public DateTime CheckOutTime { get; set; }
         public decimal Price { get; set; }
